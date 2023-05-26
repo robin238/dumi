1625289a043a499295739f58ab3fa40dc6c99404
DUMI API <50389565+robin238@users.noreply.github.com>
2023-05-26 15:44:57 +0700

    update beranda

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,17 +1,18 @@
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
- <script src="assets/js/jquery-min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.countdown.min.js"></script>
-    <script src="assets/js/jquery.nav.js"></script>
-    <script src="assets/js/jquery.easing.min.js"></script>
-    <script src="assets/js/wow.js"></script>
-    <script src="assets/js/jquery.slicknav.js"></script>
-    <script src="assets/js/nivo-lightbox.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/form-validator.min.js"></script>
-    <script src="assets/js/contact-form-script.min.js"></script>
-    <script src="assets/js/map.js"></script>
-    <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyCsa2Mi2HqyEcEnM1urFSIGEpvualYjwwM">
-    </script>
-    <script src="https://example.com/fontawesome/v6.4.0/js/fontawesome.js" data-auto-replace-svg="nest"></script>
+<script src="assets/js/jquery-min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.countdown.min.js"></script>
+<script src="assets/js/jquery.nav.js"></script>
+<script src="assets/js/jquery.easing.min.js"></script>
+<script src="assets/js/wow.js"></script>
+<script src="assets/js/jquery.slicknav.js"></script>
+<script src="assets/js/nivo-lightbox.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/form-validator.min.js"></script>
+<script src="assets/js/contact-form-script.min.js"></script>
+<script src="assets/js/map.js"></script>
+<script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyCsa2Mi2HqyEcEnM1urFSIGEpvualYjwwM">
+</script>
+<script src="https://example.com/fontawesome/v6.4.0/js/fontawesome.js" data-auto-replace-svg="nest"></script>
+<script src="assets/magicscroll/magicscroll.js"></script>
--- app/Views/header.php
+++ app/Views/header.php
@@ -1,30 +1,33 @@
  <!-- Required meta tags -->
  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>DUMI - Pinjaman Segala Kebutuhan</title>
-    <meta name="description" content="Unika - Responsive One Page HTML5 Template">
-    <meta name="keywords" content="HTML5, Bootsrtrap, One Page, Responsive, Template, Portfolio" />
-    <meta name="author" content="imransdesign.com">
-    <link rel="icon" type="image/x-icon" href="../assets/img/dumi/favicon_dumi.png">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <title>DUMI - Pinjaman Segala Kebutuhan</title>
+ <meta name="description" content="Unika - Responsive One Page HTML5 Template">
+ <meta name="keywords" content="HTML5, Bootsrtrap, One Page, Responsive, Template, Portfolio" />
+ <meta name="author" content="imransdesign.com">
+ <link rel="icon" type="image/x-icon" href="../assets/img/dumi/favicon_dumi.png">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-    <!-- Icon -->
-    <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
-    <!-- Slicknav -->
-    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
-    <!-- Nivo Lightbox -->
-    <link rel="stylesheet" type="text/css" href="assets/css/nivo-lightbox.css">
-    <!-- Animate -->
-    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
-    <!-- Main Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
-    <!-- Responsive Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+ <!-- Icon -->
+ <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
+ <!-- Slicknav -->
+ <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+ <!-- Nivo Lightbox -->
+ <link rel="stylesheet" type="text/css" href="assets/css/nivo-lightbox.css">
+ <!-- Animate -->
+ <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+ <!-- Main Style -->
+ <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+ <!-- Responsive Style -->
+ <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Quicksand:wght@300&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Quicksand:wght@300&display=swap"
+     rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+     crossorigin="anonymous" referrerpolicy="no-referrer" />
+ <link rel="stylesheet" href="assets/magicscroll/magicscroll.css" />
